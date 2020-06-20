6e1a7c4eb02bde97254bf8b41bb3931778b11a54
Robertaprog <67172924+Robertaprog@users.noreply.github.com>
2020-06-19 23:20:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y, z, maiorXY, maior;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            z = int.Parse(vet[2]);
+
+            maiorXY = (x + y + Math.Abs(x - y)) / 2;
+
+            maior = (maiorXY + z + Math.Abs(maiorXY - z)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
